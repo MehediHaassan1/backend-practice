6dc408cfd9189b62d8b990d8edc6b9005815e54a
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-05-16 14:39:11 +0600

    a single student is getting

--- src/app/modules/student/student.controllers.ts
+++ src/app/modules/student/student.controllers.ts
@@ -1,9 +1,9 @@
 import { Request, Response } from "express";
 import { StudentServices } from "./student.service";
 
-const createStudent = async(req: Request, res: Response) =>{
-    try{    
-        const {student: studentData} = req.body;
+const createStudent = async (req: Request, res: Response) => {
+    try {
+        const { student: studentData } = req.body;
         const result = await StudentServices.createStudentIntoDB(studentData);
         res.status(200).json({
             success: true,
@@ -11,25 +11,40 @@ const createStudent = async(req: Request, res: Response) =>{
             data: result,
         })
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 }
 
-const getAllStudents = async(req:Request, res: Response) =>{
-    try{
+const getAllStudents = async (req: Request, res: Response) => {
+    try {
         const result = await StudentServices.getAllStudentsFromDB();
         res.status(200).json({
             success: true,
             message: "All students were successfully retrieved",
             data: result,
         })
-    } catch(err){
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const getSingleStudent = async (req: Request, res: Response) => {
+    try {
+        const studentId = req.params.studentId;
+        const result = await StudentServices.getSingleStudentsFromDB(studentId)
+        res.status(200).json({
+            success: true,
+            message: "Student is getting successfully",
+            data: result
+        })
+    } catch (err) {
         console.log(err);
     }
 }
 
 export const StudentControllers = {
     createStudent,
-    getAllStudents
+    getAllStudents,
+    getSingleStudent
 }
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -11,7 +11,13 @@ const getAllStudentsFromDB = async () => {
     return result;
 }
 
+const getSingleStudentsFromDB = async(studentId: string) =>{
+    const result = await StudentModel.findOne({id: studentId})
+    return result;
+}
+
 export const StudentServices = {
     createStudentIntoDB,
-    getAllStudentsFromDB
+    getAllStudentsFromDB,
+    getSingleStudentsFromDB
 }
